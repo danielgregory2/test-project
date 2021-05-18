48403c2d6d67269e6ec4b784104a3d8ff09c937e
danielgregory2 <daniel.gregory@diffblue.com>
2021-05-18 12:45:01 +0100

    First commit

---
+++ src/main/java/io/diffblue/MyClassUnderTest.java
@@ -0,0 +1,8 @@
+// Copyright 2021 Diffblue Limited. All Rights Reserved.
+// Unpublished proprietary source code.
+// Use is governed by https://docs.diffblue.com/licenses/eula
+package io.diffblue;
+
+public class MyClassUnderTest {
+
+}
